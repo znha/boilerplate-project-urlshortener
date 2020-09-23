30c5029c4dfa0917084ad3f0fb05993f5c17ee61
znha <zihna-19@hotmail.com>
2020-09-23 18:17:22 +0300

    creating a full stack  short url creator

--- server.js
+++ server.js
@@ -3,17 +3,27 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const dns = require('dns');
+
 
 var cors = require('cors');
 
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
-
+/** this project needs a db !! **/
+//process.env.DB_URI
+mongoose.connect("mongodb+srv://znha:zCMSOibubd0BPGB2@cluster0.gw1av.mongodb.net/shortUrls?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true });
+var { Schema } = mongoose;
+var myUrlSchema = Schema({
+  original: String,
+  newUrl: String
+});
+var urlModel = mongoose.model('url', myUrlSchema);
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
@@ -21,17 +31,44 @@ app.use(cors());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl/new', function (req, res) {
+  var url = req.body.url;
+  console.log
+  const options = {
+    family: 6,
+    hints: dns.ADDRCONFIG | dns.V4MAPPED,
+  };
+  options.all = true;
+  dns.lookup(new URL(url).hostname, async function (err, addresses) {
+    if (err) {
+      res.json({ "error": "invalid URL" });
+    } else {
+      var newUrl = Math.floor(Math.random() * Math.floor(1000));
+      var myUrl = new urlModel({
+        original: url,
+        newUrl: newUrl
+      });
+      await myUrl.save(function (err, data) {
+        if (err) {
+          res.json({ "error": "invalid URL" });
+        }
+        res.json({ "original_url": url, "short_url": newUrl });
+      });
+    }
+  });
 
+
+});
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
